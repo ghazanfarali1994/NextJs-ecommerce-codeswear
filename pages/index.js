e747d08f87d02c880e62573d4895e7740541dd71
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-06-26 00:38:05 +0500

    adding tshirts data 37

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <Navbar/>
+      
       <div>
         <img src="/home.jpg" alt="" />
       </div> 
       <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Wear the Code with codeswear.com</h1>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Wear the Code with Codeswear.com</h1>
       <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Wear whatever you want? What do you want? You want code? so why not wear the code??</p>
     </div>
     <div className="flex flex-wrap -m-4">
@@ -100,7 +100,7 @@ export default function Home() {
   </div>
 </section>
       
-      <Footer/>
+      
     
     </>
   )
