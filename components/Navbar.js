17077df4eeb23ef8435d3efab2cf2c8f7477b7d1
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-07-23 13:58:20 +0500

    mongodb 46

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,16 +3,17 @@ import Link from 'next/link'
 import React, { useRef } from 'react'
 import { AiOutlineShoppingCart, AiFillCloseCircle, AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
 import { BsFillBagCheckFill } from 'react-icons/bs';
+import { MdAccountCircle } from 'react-icons/md';
 
 
-const Navbar = ({cart, addToCart, removeFromCart, clearCart, subTotal}) => {
+const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
   // console.log(cart, addToCart, removeFromCart, clearCart, subTotal)
   const toggleCart = () => {
-    if(ref.current.classList.contains('translate-x-full')){
+    if (ref.current.classList.contains('translate-x-full')) {
       ref.current.classList.remove('translate-x-full')
       ref.current.classList.add('translate-x-0')
     }
-    else if(!ref.current.classList.contains('translate-x-full')){
+    else if (!ref.current.classList.contains('translate-x-full')) {
       ref.current.classList.remove('translate-x-0')
       ref.current.classList.add('translate-x-full')
     }
@@ -31,31 +32,34 @@ const Navbar = ({cart, addToCart, removeFromCart, clearCart, subTotal}) => {
           <Link href={'/mugs'}><li>Mugs</li></Link>
         </ul>
       </div>
-      <div onClick={toggleCart} className="cursor-pointer cart absolute right-0 top-4 mx-5">
-        <AiOutlineShoppingCart className='text-xl md:text-2xl'/>
+      <div className="cursor-pointer cart absolute right-0 top-4 mx-5 flex">
+        <Link href={'/login'}><MdAccountCircle className='text-xl md:text-2xl mx-2' /></Link>
+        <AiOutlineShoppingCart onClick={toggleCart} className='text-xl md:text-2xl' />
       </div>
 
-      <div ref={ref} className={`w-72 h-[100vh] sideCart absolute top-0 right-0 bg-pink-100 px-8 py-10 transform transition-transform ${Object.keys(cart).length !==0 ? 'translate-x-0' : 'translate-x-full'} `}>
+      <div ref={ref} className={`w-72 h-[100vh] sideCart absolute top-0 right-0 bg-pink-100 px-8 py-10 transform transition-transform ${Object.keys(cart).length !== 0 ? 'translate-x-0' : 'translate-x-full'} `}>
         <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
-        <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"><AiFillCloseCircle/></span>
+        <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"><AiFillCloseCircle /></span>
         <ol className='list-decimal font-semibold'>
-          {Object.keys(cart).length==0 && <div className='my-4 font-semibold'>Your cart is Empty!</div> }
-          {Object.keys(cart).map((k)=>{return <li key={k}>
-            <div className="item flex my-5">
-              <div className='w-2/3 font-semibold'>Tshirt - Wear the code</div>
+          {Object.keys(cart).length == 0 && <div className='my-4 font-semibold'>Your cart is Empty!</div>}
+          {Object.keys(cart).map((k) => {
+            return <li key={k}>
+              <div className="item flex my-5">
+                <div className='w-2/3 font-semibold'>Tshirt - Wear the code</div>
                 <div className='flex w-1/3 font-semibold items-center justify-center text-lg '>
-                  <AiFillMinusCircle onClick={()=>{removeFromCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)}} className='cursor-pointer text-pink-500'/> 
-                    <span className='mx-2 text-sm'>{cart[k].qty}</span>
-                  <AiFillPlusCircle onClick={()=>{addToCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)}} className='cursor-pointer text-pink-500'/>
+                  <AiFillMinusCircle onClick={() => { removeFromCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant) }} className='cursor-pointer text-pink-500' />
+                  <span className='mx-2 text-sm'>{cart[k].qty}</span>
+                  <AiFillPlusCircle onClick={() => { addToCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant) }} className='cursor-pointer text-pink-500' />
                 </div>
-            </div>
-          </li>})}
-          
+              </div>
+            </li>
+          })}
+
         </ol>
         <div className="font-bold my-2">Subtotal: Rs.{subTotal} </div>
 
         <div className="flex">
-          <Link href={'/checkout'}><button className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm"><BsFillBagCheckFill className='m-1'/>Checkout</button></Link>
+          <Link href={'/checkout'}><button className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm"><BsFillBagCheckFill className='m-1' />Checkout</button></Link>
           <button onClick={clearCart} className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm">Clear Cart</button>
         </div>
       </div>
