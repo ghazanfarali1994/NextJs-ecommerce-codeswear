7926da42256354594afd36bb24a7542ab4338346
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-09-05 22:59:03 +0500

    customizing hoodies, mugs and stickers page 53

--- models/User.js
+++ models/User.js
@@ -7,5 +7,6 @@ const UserSchema = new mongoose.Schema({
     
 }, {timestamps: true});
 
-mongoose.models = {}
-export default mongoose.model("Product", UserSchema);
+// mongoose.models = {}
+// export default mongoose.model("User", UserSchema);
+export default mongoose.models.User || mongoose.model("User", UserSchema);
