22a7a3b07f07123c90e6d885f45d6087eb0e81f4
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-09-21 23:09:00 +0500

    env 61

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
   const [cart, setCart] = useState({})
   const [subTotal, setSubTotal] = useState(0)
   const [user, setUser] = useState({value: null})
-  const [key, setKey] = useState(0)
+  const [key, setKey] = useState()
   
   const [progress, setProgress] = useState(0)
   
